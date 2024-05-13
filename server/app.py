93bcb9410fdcb3552005e28f7234a85725b43e3b
GYUHWAN JEONG <easter721@gmail.com>
2024-05-13 19:52:18 +0900

    fix : connection rasa

--- server/app.py
+++ server/app.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/data')
-def get_data():
-    data = {'message': 'Hello from Flask!'}
-    return jsonify(data)
+@app.route('/')
+def home():
+    return "Home page"
+
+@app.route('/send', methods=['POST'])
+def send_message_to_rasa():
+    user_message = request.json['message']
+    headers = {'Content-Type': 'application/json'}
+    
+    response = requests.post('http://localhost:5005/webhooks/rest/webhook', json={"sender": "user", "message": user_message}, headers=headers)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"message": "Rasa 서버로부터 응답을 받지 못했습니다."})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
