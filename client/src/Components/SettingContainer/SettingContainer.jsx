94ade7f2ab8b757712f04b03f14ad4b7da1d73fe
GYUHWAN JEONG <easter721@gmail.com>
2024-05-22 18:57:51 +0900

    feat : add settingContainer Components

--- client/src/Components/SettingContainer/SettingContainer.jsx
+++ client/src/Components/SettingContainer/SettingContainer.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import "./Styles/SettingContainer.css";
 
-export default function SettingContainer({ settings }) {
+export const UserContainer = ({ userSettings }) => {
   return (
     <div className="settingContainer">
       <div className="profile">
-        <h1>{settings.profileTitle}</h1>
-        <p>이름: {settings.name}</p>
-        <p>성별: {settings.gender}</p>
+        <h1>{userSettings.profileTitle}</h1>
+        <p>이름: {userSettings.name}</p>
+        <p>성별: {userSettings.gender}</p>
+        <p>{userSettings.class}</p>
       </div>
       <div className="setting">
-        <h1>{settings.settingTitle}</h1>
+        <h1>{userSettings.settingTitle}</h1>
       </div>
       <div className="settingBtn">
-        <button>{settings.buttonText}</button>
+        <button className="addBtn">{userSettings.buttonText}</button>
       </div>
     </div>
   );
-}
+};
+
+export const PersonaContainer = ({ personaSettings }) => {
+  return (
+    <div className="settingContainer">
+      <div className="profile">
+        <h1>{personaSettings.profileTitle}</h1>
+        <p>이름: {personaSettings.name}</p>
+        <p>성별: {personaSettings.gender}</p>
+        <p>{personaSettings.class}</p>
+      </div>
+      <div className="setting">
+        <h1>{personaSettings.settingTitle}</h1>
+      </div>
+      <div className="settingBtn">
+        <button className="addBtn">{personaSettings.buttonText}</button>
+      </div>
+    </div>
+  );
+};
