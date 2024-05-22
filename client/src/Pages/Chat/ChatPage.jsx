521ea73d33399077b72130df3b594dd6129fc6ab
GYUHWAN JEONG <easter721@gmail.com>
2024-05-22 18:58:23 +0900

    fix : fix chatlayout

--- client/src/Pages/Chat/ChatPage.jsx
+++ client/src/Pages/Chat/ChatPage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../../Layouts/Header/Header";
-import SettingContainer from "../../Components/SettingContainer/SettingContainer.jsx";
+import {
+  UserContainer,
+  PersonaContainer,
+} from "../../Components/SettingContainer/SettingContainer.jsx";
+import Test from "../../Components/Test.jsx";
 import "./Styles/Chat.css";
 
 export default function ChatPage() {
@@ -14,23 +18,23 @@ export default function ChatPage() {
   };
 
   const personaSettings = {
-    title: "페르소나 설정",
-    profileTitle: "페르소나 머리",
-    settingTitle: "페르소나 설정",
-    buttonText: "버튼2",
-    name: "페르소나 이름",
-    gender: "여성",
+    title: "사용자 설정",
+    profileTitle: "페르소나",
+    settingTitle: "설정",
+    buttonText: "버튼1",
+    name: "사용자 이름",
+    gender: "남성",
   };
 
   return (
     <>
       <Header />
       <div className="mainChatContainer">
-        <SettingContainer settings={userSettings} />
+        <UserContainer userSettings={userSettings} />
         <div className="chatContainer">
-          <h1>채팅</h1>
+          <Test />
         </div>
-        <SettingContainer settings={personaSettings} />
+        <PersonaContainer personaSettings={personaSettings} />
       </div>
     </>
   );
